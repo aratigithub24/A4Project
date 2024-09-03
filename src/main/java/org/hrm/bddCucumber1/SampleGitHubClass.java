9a974298b7f52b4f21a97da6b4ab74b402aa6c76
Itishree Arati Jena <jarati9178@gmail.com>
2024-09-03 14:50:28 +0530

    commit 3

---
+++ src/main/java/org/hrm/bddCucumber1/SampleGitHubClass.java
@@ -0,0 +1,8 @@
+package org.hrm.bddCucumber1;
+
+public class SampleGitHubClass {
+
+	public void m1() {
+		System.out.println("sample class");
+	}
+}
---
+++ src/test/java/org/hrm/bddCucumber2/SampleCode.java
@@ -0,0 +1,10 @@
+package org.hrm.bddCucumber2;
+
+public class SampleCode 
+{
+		public void m1() {
+			System.out.println("sample class");
+		}
+	
+
+}
